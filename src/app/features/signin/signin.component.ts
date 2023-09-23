537cdb23ab039e203c6a5e1e9c9f2cf0083ee461
enderson <enderson.linhares20@gmail.com>
2023-09-23 12:00:26 -0300

    feat: structure project and add files

---
+++ src/app/features/signin/signin.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.sass']
+})
+export class SigninComponent {
+}
