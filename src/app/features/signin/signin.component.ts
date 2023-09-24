10da35384c6ccdad3cc53ad0da7ed8fee4968b91
enderson <enderson.linhares20@gmail.com>
2023-09-23 21:09:00 -0300

    feat: configuring inputs on `signin` and `signup`

--- src/app/features/signin/signin.component.ts
+++ src/app/features/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-signin',
@@ -6,4 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./signin.component.sass']
 })
 export class SigninComponent {
+  formulario!: FormGroup
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(){
+    this.formulario = this.formBuilder.group({
+      email: [null, Validators.email],
+      password: [null]
+    })
+  }
+
+  onSubmit(){
+    console.log(this.formulario.value)
+  }
+
 }
--- src/app/features/signup/signup.component.ts
+++ src/app/features/signup/signup.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.sass']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  signUpForm!: FormGroup
+
+  constructor(
+    private formBuild: FormBuilder
+  ) {
+  }
+
+  onSubmit() {
+
+  }
+
+  ngOnInit() {
+    this.signUpForm = this.formBuild.group({
+      nome: [null],
+      email: [null, Validators.email],
+      senha: [null],
+      confirmacaoSenha: [null],
+    })
+  }
 
 }
