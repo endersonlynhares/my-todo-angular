b72813ed4b2630d5f7cbce4ebe6ef0173021ef48
enderson <enderson.linhares20@gmail.com>
2023-09-24 11:41:45 -0300

    feat: adding forms validation in the `signup`component

--- src/app/features/signup/signup.component.ts
+++ src/app/features/signup/signup.component.ts
@@ -1,29 +1,31 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FormValidations} from "../../shared/form-validations";
+import {BaseFormComponent} from "../../shared/form-base/form-base";
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.sass']
 })
-export class SignupComponent implements OnInit {
-  signUpForm!: FormGroup
+export class SignupComponent extends BaseFormComponent implements OnInit {
 
   constructor(
     private formBuild: FormBuilder
   ) {
+    super()
   }
 
-  onSubmit() {
-
+  override submit() {
+    console.log(this.formulario)
   }
 
-  ngOnInit() {
-    this.signUpForm = this.formBuild.group({
-      nome: [null],
-      email: [null, Validators.email],
-      senha: [null],
-      confirmacaoSenha: [null],
+  override ngOnInit() {
+    this.formulario = this.formBuild.group({
+      nome: [null, [Validators.required]],
+      email: [null, [Validators.required, Validators.email]],
+      senha: [null, [Validators.required, Validators.minLength(8)]],
+      confirmacaoSenha: [null, [Validators.required, FormValidations.equalsTo('senha')]],
     })
   }
 
--- src/app/shared/form-validations.ts
+++ src/app/shared/form-validations.ts
@@ -1,12 +1,36 @@
+import {FormControl, FormGroup} from "@angular/forms";
+
 export class FormValidations {
+
+  static equalsTo(fieldToCompare: string) {
+    return (formControl: FormControl) => {
+      if (fieldToCompare == null) {
+        throw new Error('É necessário informar o campo!')
+      }
+      if (!formControl || !(<FormGroup>formControl.root).controls) {
+        return null
+      }
+
+      const field = (<FormGroup>formControl.root).get(fieldToCompare)
+
+      if (!field) {
+        throw new Error('É necessário informar um campo válido!')
+      }
+      if (field.value !== formControl.value) {
+        return {equalsTo: fieldToCompare}
+      }
+
+      return null
+    }
+  }
+
   static getErrorMsg(fieldName: string, validatorName: string, validatorValue?: any) {
     const config: any = {
-      'required': `${fieldName} é obrigatório.`,
+      'required': `O campo é obrigatório, por favor digite.`,
       'minlength': `${fieldName} precisa ter no mínimo ${validatorValue.requiredLength} caracteres.`,
       'maxlength': `${fieldName} precisa ter no máximo ${validatorValue.requiredLength} caracteres.`,
-      'cepInvalido': 'CEP inválido.',
       'email': 'email inválido',
-      'equalsTo': 'campos não são iguais',
+      'equalsTo': 'os campos não são iguais',
       'pattern': 'campo inválido'
     };
 
