8b7e97ca4a1373c9dea2a310ce84d5aed1929b86
endersonlynhares <enderson.linhares20@gmail.com>
2023-10-11 16:13:05 -0300

    feat: create files to dialog-template

--- src/app/core/components/components.module.ts
+++ src/app/core/components/components.module.ts
@@ -3,10 +3,12 @@ import {CommonModule, NgOptimizedImage} from '@angular/common';
 import {SidenavComponent} from './sidenav/sidenav.component';
 import {RouterLink, RouterLinkActive} from "@angular/router";
 import {SharedModule} from "../../shared/shared.module";
+import {DialogTemplateComponent} from "./dialog-template/dialog-template";
 
 @NgModule({
   declarations: [
     SidenavComponent,
+    DialogTemplateComponent
   ],
   imports: [
     CommonModule,
