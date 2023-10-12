9c4fe829f1a12a7f2d07ed11f7c739fc66acf82e
enderson <enderson.linhares20@gmail.com>
2023-10-12 11:16:13 -0300

    feat: put pagination on table and starting on infinite scroll.

--- src/app/shared/dialogs/create-task-dialog/create-task-dialog.component.ts
+++ src/app/shared/dialogs/create-task-dialog/create-task-dialog.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, HostListener, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {FormBuilder, Validators} from "@angular/forms";
 import {ApiService} from "../../../core/services/api.service";
 import {BaseFormComponent} from "../../form-base/form-base";
@@ -6,6 +6,7 @@ import {AssignmentList} from "../../../domain-types/models/AssigmentList";
 import {Subscription} from "rxjs";
 import {FormValidations} from "../../form-validations";
 import * as moment from "moment"
+import {MatSelect} from "@angular/material/select";
 
 @Component({
   selector: 'app-create-task-dialog',
@@ -14,13 +15,33 @@ import * as moment from "moment"
 })
 export class CreateTaskDialogComponent extends BaseFormComponent implements OnDestroy, OnInit {
   lists !: AssignmentList[]
+  showMore: boolean = false
   private subscription!: Subscription
+  page = 1
+
 
   constructor(
     private buildr: FormBuilder,
     private api: ApiService
   ) {
     super()
+  }
+
+  @ViewChild('matSelect', { static: false }) matSelect!: MatSelect;
+
+  scrollHandler(event: Event) {
+    let insightsResults = document.getElementsByClassName('insights-results')[0];
+    let childInsights = insightsResults?.scrollHeight;
+    let elementScroll = this.matSelect?.panel.nativeElement.scrollTop;
+
+    if (elementScroll + this.matSelect?.panel.nativeElement.clientHeight >= childInsights) {
+      this.loadMore();
+      console.log('loading...', this.page);
+    }
+  }
+
+  loadMore(): void {
+    this.page++;
   }
 
   override ngOnInit() {
@@ -35,6 +56,9 @@ export class CreateTaskDialogComponent extends BaseFormComponent implements OnDe
     this.subscription = this.api.getAssignmentList().subscribe({
       next: (data) => {
         this.lists = data.items
+        if (data.perPage < data.total) {
+          this.showMore = true
+        }
       }
     })
   }
