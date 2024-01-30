f9db693d50fc246ac9569a31913bfacec3e87940
endersonlynhares <enderson.linhares20@gmail.com>
2024-01-30 16:58:17 -0300

    feat: using angular material theming

--- src/app/core/components/sidenav/sidenav.component.ts
+++ src/app/core/components/sidenav/sidenav.component.ts
@@ -1,8 +1,9 @@
-import {Component} from '@angular/core';
+import {Component, HostBinding} from '@angular/core';
 import {sideNavData} from "./sidenav-data";
 import {AuthService} from "../../services/auth.service";
 import {User} from "../../../domain-types/models/User";
 import {ThemeService} from "../../theme/theme.service";
+import {Overlay, OverlayContainer} from "@angular/cdk/overlay";
 
 @Component({
   selector: 'app-sidenav',
@@ -17,7 +18,8 @@ export class SidenavComponent {
 
   constructor(
     private authService: AuthService,
-    private themeService: ThemeService
+    private themeService: ThemeService,
+    private overlay: OverlayContainer
   ) {
     this.user = authService.getUser()
     this.activeTheme = this.themeService.getActiveTheme().name
@@ -31,13 +33,21 @@ export class SidenavComponent {
     this.authService.logoutUser()
   }
 
-
+  @HostBinding('class') className = ""
+  darkClassName = "theme-dark"
+  lightClassName = "theme-light"
   toggle() {
     const active = this.themeService.getActiveTheme();
     if (active.name === 'light') {
       this.themeService.setTheme('dark');
+      this.className = this.darkClassName
+      this.overlay.getContainerElement().classList.add(this.darkClassName)
+      this.overlay.getContainerElement().classList.remove(this.lightClassName)
     } else {
       this.themeService.setTheme('light');
+      this.className = this.lightClassName
+      this.overlay.getContainerElement().classList.add(this.lightClassName)
+      this.overlay.getContainerElement().classList.remove(this.darkClassName)
     }
     this.activeTheme = this.themeService.getActiveTheme().name
   }
